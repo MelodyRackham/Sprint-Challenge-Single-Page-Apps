5417529e9f25762efc7bdbc33645de2bebf54535
Mel Maki <melody.rackham@gmail.com>
2019-10-18 12:42:33 -0600

    Got search bar to actually work

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -11,18 +11,40 @@ const CharactersHead = styled.h1`
 
 const RAndMortyList = () => {
   const [Characters, setCharacters] = useState([]);
+  const [query, setQuery] = useState('');
+  const [filteredCharacter, setFilteredCharacter] = useState([]);
+
+  // useEffect(() => {
+  //   axios.get(`https://api.pokemontcg.io/v1/cards`).then(response => {
+  //     console.log(response);
+  //     setCharacters(response.data.cards);
+  //   });
+  // }, []);
 
   useEffect(() => {
-    axios.get(`https://api.pokemontcg.io/v1/cards`).then(response => {
-      console.log(response);
-      setCharacters(response.data.cards);
-    });
-  }, []);
+    setFilteredCharacter(
+      Characters.filter(character => character.name.toLowerCase().includes(query.toLocaleLowerCase())),
+    );
+  }, [query, Characters]);
+  const handleInputChange = event => {
+    setQuery(event.target.value);
+  };
 
   return (
     <div>
+      <h1> Search Below for your Favorite Character</h1>
+      <form className='search'>
+        <input
+          type='text'
+          onChange={handleInputChange}
+          value={query}
+          name='name'
+          placeholder='search by name'
+          autoComplete='off'
+        />
+      </form>
       <CharactersHead> Characters </CharactersHead>
-      {Characters.map(character => {
+      {filteredCharacter.map(character => {
         return <CharacterCard key={character.id} name={character.name} url={character.imageUrl} />;
       })}
     </div>
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,51 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 
 const SearchForm = () => {
-  const [data, setData] = useState([]);
-  const [query, setQuery] = useState('');
-  const [filteredCharacter, setFilteredCharacter] = useState([]);
-
-  useEffect(() => {
-    axios.get(`https://pokeapai.co/aip/v2/pokemon`).then(response => {
-      console.log(response.data.results);
-      setData(response.data.results);
-      setFilteredCharacter(response.data.results);
-    });
-  }, []);
-
-  useEffect(() => {
-    setFilteredCharacter(data.filter(character => character.name.toLowerCase().includes(query.toLocaleLowerCase())));
-  }, [query, data]);
-  const handleInputChange = event => {
-    setQuery(event.target.value);
-  };
-
   return (
     <section className='search-form'>
-      <h1> Search Below for your Favorite Character </h1>
       <Link to='/CharacterList'></Link>
-      <form className='search'>
-        <input
-          type='text'
-          onChange={handleInputChange}
-          value={query}
-          name='name'
-          placeholder='search by name'
-          autoComplete='off'
-        />
-      </form>
-      <div>
-        {filteredCharacter.map(data => {
-          return (
-            <div className='character-list' key={data.id}>
-              {console.log(data.url)}
-              <h2>{data.name}</h2>
-            </div>
-          );
-        })}
-      </div>
+
+      <div></div>
     </section>
   );
 };
