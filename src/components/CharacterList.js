2983c5a0ba15834803af2e2c9324b9202c056581
Mel Maki <melody.rackham@gmail.com>
2019-10-18 11:36:42 -0600

    Added a router to the application using react router to link the home page and character page

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -3,20 +3,27 @@ import axios from 'axios';
 import CharacterCard from './CharacterCard';
 import styled from 'styled-components';
 
+const CharactersHead = styled.h1`
+  color: red;
+  font-size: 4rem;
+  font-family: 'Long Cang', cursive;
+`;
+
 const RAndMortyList = () => {
   const [Characters, setCharacters] = useState([]);
 
   useEffect(() => {
-    axios.get(`https://rickandmortyapi.com/api/character/`).then(response => {
-      console.log(response.data.results);
-      setCharacters(response.data.results);
-    }, []);
-  });
+    axios.get(`https://api.pokemontcg.io/v1/cards`).then(response => {
+      console.log(response);
+      setCharacters(response.data.cards);
+    });
+  }, []);
 
   return (
     <div>
+      <CharactersHead> Characters </CharactersHead>
       {Characters.map(character => {
-        return <CharacterCard key={character.id} name={character.name} url={character.image} />;
+        return <CharacterCard key={character.id} name={character.name} url={character.imageUrl} />;
       })}
     </div>
   );
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-export default function SearchForm() {
- 
+const SearchForm = () => {
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState('');
+  const [filteredCharacter, setFilteredCharacter] = useState([]);
+
+  useEffect(() => {
+    axios.get(`https://pokeapai.co/aip/v2/pokemon`).then(response => {
+      console.log(response.data.results);
+      setData(response.data.results);
+      setFilteredCharacter(response.data.results);
+    });
+  }, []);
+
+  useEffect(() => {
+    setFilteredCharacter(data.filter(character => character.name.toLowerCase().includes(query.toLocaleLowerCase())));
+  }, [query, data]);
+  const handleInputChange = event => {
+    setQuery(event.target.value);
+  };
+
   return (
-    <section className="search-form">
-     // Add a search form here
+    <section className='search-form'>
+      <h1> Welcome to Mel's Search Bar</h1>
+      <Link to='/CharacterList'>
+        <h2> I love Ricky and Morty </h2>
+      </Link>
+      <h3> I am creating a search bar </h3>
+      <form className='search'>
+        <input
+          type='text'
+          onChange={handleInputChange}
+          value={query}
+          name='name'
+          placeholder='search by name'
+          autoComplete='off'
+        />
+      </form>
+      <div>
+        {filteredCharacter.map(data => {
+          return (
+            <div className='character-list' key={data.id}>
+              {console.log(data.url)}
+              <h2>{data.name}</h2>
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
-}
+};
+
+// export default SearchForm;
